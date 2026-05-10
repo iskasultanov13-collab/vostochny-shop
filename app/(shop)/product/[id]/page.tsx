e2c80fea30a1492f4842b9e30ec3f13debc5743b
iskasultanov13-collab <iskasultanov13@gmail.com>
2026-05-10 11:13:34 +0500

    Update page.tsx

--- app/(shop)/product/[id]/page.tsx
+++ app/(shop)/product/[id]/page.tsx
@@ -2,10 +2,9 @@ import { getProductById } from '@/lib/products'
 import { notFound } from 'next/navigation'
 import { ProductDetailClient } from './ProductDetailClient'
 
-interface PageProps { params: { id: string } }
-
-export default async function ProductPage({ params }: PageProps) {
-  const product = await getProductById(params.id)
+export default async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+  const product = await getProductById(id)
   if (!product) notFound()
   return <ProductDetailClient product={product} />
 }
